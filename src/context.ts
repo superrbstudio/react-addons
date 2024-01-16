80829838c62e512ffe12b265af7dcb221614bbf2
Yvette Leppard <yvette@superrb.com>
2024-01-16 14:30:57 +0000

    migrate cookies context to zustand

--- src/context.ts
+++ src/context.ts
@@ -2,18 +2,12 @@
  * Context
  */
 import {
-  CookiesContext,
-  CookiesContextProvider,
-} from './context/cookies-context-provider'
-import {
   ModalContext,
   ModalContextProvider,
 } from './context/modal-context-provider'
 import { NavContext, NavContextProvider } from './context/nav-context-provider'
 
 export {
-  CookiesContext,
-  CookiesContextProvider,
   ModalContext,
   ModalContextProvider,
   NavContext,
