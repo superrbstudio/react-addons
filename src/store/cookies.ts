3864519928760b51ccddd169af376630fe549dc1
Yvette Leppard <yvette@superrb.com>
2024-01-16 14:30:57 +0000

    migrate cookies context to zustand

---
+++ src/store/cookies.ts
@@ -0,0 +1,41 @@
+import { create } from 'zustand'
+import Cookies from 'js-cookie'
+
+export interface CookieState {
+  cookiesAccepted: boolean
+  trackingCookiesAccepted: boolean
+  setCookiesAccepted: (accepted: boolean) => void
+  setTrackingCookiesAccepted: (accepted: boolean) => void
+  popupOpen: boolean
+  openPopup: () => void
+  closePopup: () => void
+}
+
+const useCookieStore = create<CookieState>()((set) => {
+  const cookiesAccepted = !!Cookies.get('accepted-cookies') || false
+  const trackingCookiesAccepted =
+    !!Cookies.get('accepted-tracking-cookies') || false
+  return {
+    cookiesAccepted,
+    trackingCookiesAccepted,
+    setCookiesAccepted: (accepted: boolean) => {
+      Cookies.set('accepted-cookies', accepted.toString(), {
+        expires: 30,
+      })
+      return set((state: CookieState) => ({ cookiesAccepted: accepted }))
+    },
+    setTrackingCookiesAccepted: (accepted: boolean) => {
+      Cookies.set('accepted-tracking-cookies', accepted.toString(), {
+        expires: 30,
+      })
+      return set((state: CookieState) => ({
+        trackingCookiesAccepted: accepted,
+      }))
+    },
+    popupOpen: false,
+    openPopup: () => set({ popupOpen: true }),
+    closePopup: () => set({ popupOpen: false }),
+  }
+})
+
+export default useCookieStore
