a280d7bfa6e7006f061b71e68925ca7bc5d62d1c
James Dinsdale <hi@molovo.co>
2025-01-24 21:25:29 +0000

    Scroll success message into view on submit
    Fixes superrb/crosta-mollica-next#60

--- src/components/form/success-message.tsx
+++ src/components/form/success-message.tsx
@@ -1,10 +1,20 @@
+'use client'
+
+import { useEffect, useRef } from 'react'
+
 export default function SuccessMessage({
   data,
 }: {
   data: Record<string, any>
 }) {
+  const ref = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    ref.current?.scrollIntoView({ behavior: 'smooth' })
+  }, [])
+
   return (
-    <div className="success-message">
+    <div className="success-message" ref={ref}>
       <h2 className="success-message__title">Thanks for your message</h2>
       <p className="success-message__text">
         We'll get back to you as soon as possible.
