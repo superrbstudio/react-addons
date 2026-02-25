45d647f042c12022e11deecd3c12c1b1b0c6a39d
Giles Smith <giles@superrb.com>
2026-02-25 09:54:28 +0000

    feat: add googleTagManagerCspConfig

--- src/config.ts
+++ src/config.ts
@@ -1,17 +1,17 @@
-import { EVAL, INLINE, SELF, NONE, CSPDirectives } from 'csp-header'
+import { EVAL, INLINE, SELF, NONE, DATA, BLOB, CSPDirectives } from 'csp-header'
 
 export type CSPPresetGenerator = (...args: any[]) => Partial<CSPDirectives>
 
 export const cspConfig: CSPPresetGenerator = () => ({
   'default-src': [SELF],
   'connect-src': [SELF],
-  'script-src': [SELF, INLINE, EVAL, 'netlify-run.netlify.app'],
+  'script-src': [SELF, INLINE, EVAL],
   'style-src': [SELF, INLINE],
-  'img-src': [SELF, 'blob:', 'data:'],
-  'font-src': [SELF, 'data:'],
+  'img-src': [SELF, BLOB, DATA],
+  'font-src': [SELF, DATA],
   'object-src': [NONE],
-  'media-src': [SELF, 'blob:', 'data:'],
-  'frame-src': [SELF, 'app.netlify.com'],
+  'media-src': [SELF, BLOB, DATA],
+  'frame-src': [SELF],
   'frame-ancestors': [SELF],
   'base-uri': [SELF],
   'form-action': [SELF],
@@ -24,4 +24,52 @@ export const recaptchaCspConfig: CSPPresetGenerator = () => ({
     'https://www.gstatic.com/recaptcha/',
   ],
   'frame-src': ['https://www.google.com/recaptcha/'],
+})
+
+export const googleTagManagerCspConfig: CSPPresetGenerator = () => ({
+  'connect-src': [
+    'www.googletagmanager.com',
+    'www.google.com',
+    'https://*.google-analytics.com',
+    'https://*.analytics.google.com',
+    'https://*.googletagmanager.com',
+    'https://pagead2.googlesyndication.com',
+    'https://www.googleadservices.com',
+    'https://googleads.g.doubleclick.net',
+    'https://www.google.com',
+    'https://google.com',
+  ],
+  'script-src': [
+    'https://www.googletagmanager.com',
+    'https://googletagmanager.com',
+    'https://tagmanager.google.com',
+    'https://*.googletagmanager.com',
+    'https://www.googleadservices.com',
+    'https://www.google.com',
+    'https://pagead2.googlesyndication.com',
+    'https://googleads.g.doubleclick.net',
+  ],
+  'frame-src': [
+    'https://www.googletagmanager.com',
+  ],
+  'img-src': [
+    'www.googletagmanager.com',
+    'https://googletagmanager.com',
+    'https://ssl.gstatic.com',
+    'https://www.gstatic.com',
+    'https://*.google-analytics.com',
+    'https://*.googletagmanager.com',
+    'https://www.googletagmanager.com',
+    'https://googleads.g.doubleclick.net',
+    'https://www.google.com',
+    'https://pagead2.googlesyndication.com',
+    'https://www.googleadservices.com',
+    'https://google.com',
+  ],
+  'style-src': [
+    'https://googletagmanager.com',
+    'https://tagmanager.google.com',
+    'https://fonts.googleapis.com',
+  ],
+  'font-src': ['https://fonts.gstatic.com'],
 })
