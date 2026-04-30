d947830727157c615154cf3d59352c6601d0d21a
James Dinsdale <hi@molovo.co>
2026-04-30 11:32:24 +0100

    feat: hide automatic recaptcha message in form footer

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -422,28 +422,6 @@ const FormInner = forwardRef(function FormInner<
           )}
 
           {renderSubmit({ disabled: disabled || status === 'pending' })}
-
-          {useRecaptcha && (
-            <p className="form__recaptcha-message">
-              This site is protected by reCAPTCHA and the Google{' '}
-              <a
-                href="https://policies.google.com/privacy"
-                target="_blank"
-                rel="noopener"
-              >
-                Privacy Policy
-              </a>{' '}
-              and{' '}
-              <a
-                href="https://policies.google.com/terms"
-                target="_blank"
-                rel="noopener"
-              >
-                Terms of Service
-              </a>{' '}
-              apply.
-            </p>
-          )}
         </form>
       )}
     </>
